70cdcdb1beb16a038f0d344b553b53b996cdc313
Dennis Vinterfjärd <dennis@vinterfjard.com>
2025-05-11 16:32:59 +0200

    refactor: remove unused head section for favicon in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.png" />
-      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
